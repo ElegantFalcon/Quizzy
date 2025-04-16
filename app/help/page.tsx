ab88a90244552a876fb84a28db647ebe81afec12
Aswini <aswinipriya2004@gmail.com>
2025-04-16 21:37:42 +0530

    created help page

---
+++ app/help/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Navbar } from "@/components/navbar"
+
+function HelpPage() {
+  return (
+    <div>
+       <Navbar showBackground={true} />
+    </div>
+  )
+}
+export default HelpPage;
